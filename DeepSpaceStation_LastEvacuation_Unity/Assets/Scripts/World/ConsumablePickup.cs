9beebbffa0f5bbcca5f744918f303af59780faca
Zeyuan Huang <kinghuang035@gmail.com>
2026-05-28 12:12:41 +0800

    Add consumable pickup audio feedback

--- DeepSpaceStation_LastEvacuation_Unity/Assets/Scripts/World/ConsumablePickup.cs
+++ DeepSpaceStation_LastEvacuation_Unity/Assets/Scripts/World/ConsumablePickup.cs
@@ -22,6 +22,7 @@ public class ConsumablePickup : MonoBehaviour, IInteractable
 
         collected = true;
         interactor.Inventory.AddItem(itemName, displayName, quantity);
+        GameAudio.Instance?.PlayPickup();
         HUDController.Instance?.ShowMessage(displayName + " added to backpack", 2.4f);
 
         foreach (Renderer renderer in GetComponentsInChildren<Renderer>())
